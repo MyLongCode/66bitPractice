e89e24642551e18410698a7853729317fa5956b5
mylongcode <mylongcode@gmail.com>
2024-05-28 22:23:38 +0500

    initial commit

---
+++ footballApi/footballApi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace footballApi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
